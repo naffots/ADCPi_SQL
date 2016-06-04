8d61f82dfea82d5f2bf1d1f0f32f129d0e739ac8
Christoffer Nilsson <c32@live.se>
2016-06-04 15:08:03 +0200

    Add db if missing

---
+++ README.md
@@ -0,0 +1,6 @@
+## ADCPi_SQL project
+
+Install python3-smbus
+Activate I2C with raspi-config
+
+Run with python3 palm...
--- palm_tree_measurement.py
+++ palm_tree_measurement.py
@@ -5,6 +5,8 @@ from ABE_helpers import ABEHelpers
 import sqlite3
 import datetime
 import time
+import sys
+import os.path
 
 """
 ================================================
@@ -22,9 +24,18 @@ Sample rate can be 12,14, 16 or 18
 """
 
 db_name = 'measurements.db'
-i2c_helper = ABEHelpers()
-bus = i2c_helper.get_smbus()
-adc = ADCPi(bus, 0x68, 0x69, 18)
+
+def createdb():
+    conn = sqlite3.connect(db_name)
+    c = conn.cursor()
+    try:
+        c.execute("CREATE TABLE 'measurements' ('date' TEXT NOT NULL, 'temperature' TEXT NOT NULL, 'moisture' TEXT NOT NULL)")
+        conn.commit()
+    except sqlite3.OperationalError:
+        print("Failed to create database!")
+    
+    conn.close()
+
 
 def writetodb(temperature, moisture):
     conn = sqlite3.connect(db_name)
@@ -37,17 +48,31 @@ def writetodb(temperature, moisture):
     
     conn.close()
 
-while (True):
-    temp = 0
-    moisture = 0
+def main():
 
-    for x in range(0, 60):
-        # read from adc channels and write to the log file
-        temp = temp + (adc.read_voltage(1) - 0.5) * 100
-        moisture = moisture + adc.read_voltage(2)
-        time.sleep(0.6)
+    i2c_helper = ABEHelpers()
+    bus = i2c_helper.get_smbus()
+    adc = ADCPi(bus, 0x68, 0x69, 18)
 
-    temp = temp / 60
-    moisture =  moisture / 60
+    if not os.path.isfile(db_name):
+        createdb()
+ 
+    while (True):
+        temp = 0
+        moisture = 0
 
-    writetodb("%02f" % temp, "%02f" % moisture)
+        for x in range(0, 60):
+            # read from adc channels and write to the log file
+            temp = temp + (adc.read_voltage(1) - 0.5) * 100
+            moisture = moisture + adc.read_voltage(2)
+            time.sleep(0.6)
+
+        temp = temp / 60
+        moisture =  moisture / 60
+        print("temp,moisture")
+        print(temp)
+        print(moisture)
+        writetodb("%02f" % temp, "%02f" % moisture)
+
+if __name__ == "__main__":
+    sys.exit(main())
